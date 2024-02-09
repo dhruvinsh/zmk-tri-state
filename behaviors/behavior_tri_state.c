9e4439405ee6f3d90949dbfa00f786cbbb1f01e0
Dhruvin Shah <dhruvin3@gmail.com>
2024-02-09 18:58:48 -0500

    fix(behaviors): supports zephyr 3.5 changes

--- behaviors/behavior_tri_state.c
+++ behaviors/behavior_tri_state.c
@@ -70,7 +70,8 @@ void trigger_end_behavior(struct active_tri_state *si) {
 }
 
 void behavior_tri_state_timer_handler(struct k_work *item) {
-    struct active_tri_state *tri_state = CONTAINER_OF(item, struct active_tri_state, release_timer);
+    struct k_work_delayable *d_work = k_work_delayable_from_work(item);
+    struct active_tri_state *tri_state = CONTAINER_OF(d_work, struct active_tri_state, release_timer);
     if (!tri_state->is_active || tri_state->timer_cancelled || tri_state->is_pressed) {
         return;
     }
@@ -295,7 +296,7 @@ static int tri_state_layer_state_changed_listener(const zmk_event_t *eh) {
         .continue_behavior = _TRANSFORM_ENTRY(1, n),                                               \
         .end_behavior = _TRANSFORM_ENTRY(2, n)};                                                   \
     BEHAVIOR_DT_INST_DEFINE(n, behavior_tri_state_init, NULL, NULL,                                \
-                            &behavior_tri_state_config_##n, APPLICATION,                           \
+                            &behavior_tri_state_config_##n, POST_KERNEL,                           \
                             CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_tri_state_driver_api);
 
 DT_INST_FOREACH_STATUS_OKAY(TRI_STATE_INST)
